1efbcc9cc9e827d97e830e71eb35af4e3abb3d5d
prpr48 <prpr48@naver.com>
2020-03-01 01:59:11 +0900

    add typeDefs

---
+++ index.ts
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
